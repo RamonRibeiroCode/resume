0306e6c019af883223624834897e598cf0505131
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2023-01-21 22:43:30 -0300

    feat(gtm): adiciona tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unknown-property */
-
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import Script from "next/script"
 import { Fira_Code } from "@next/font/google"
 import { ApolloProvider } from "@apollo/client"
 
@@ -36,6 +36,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W9VNHHJ');
+      `}
+      </Script>
 
       <ApolloProvider client={client}>
         <Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
       <Head />
 
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9VNHHJ" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
